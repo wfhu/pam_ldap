ea328daf4b806749421d34498c7637be82212567
git <huwenfeng151@sohu.com>
2012-05-21 17:27:47 +0800

    git initial import

---
+++ vers.c
@@ -0,0 +1,6 @@
+/* vers.c built by $Id: vers_string,v 1.2 2010/02/25 14:40:39 lukeh Exp $ */
+
+const char SGS_VERS[160] = "@(#)PROGRAM:pam_ldap-186  PROJECT:pam_ldap-186  DEVELOPER:root  BUILT:Fri Apr 20 02:32:36 UTC 2012";
+
+const char VERS_NUM[10] = "186";
+
